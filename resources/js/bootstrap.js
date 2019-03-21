008eff90c33dbcaa46aaae568dd7221988c652e9
Raphael Mobis Tacla <r.mobis@gmail.com>
2019-03-21 01:56:23 -0300

    apply decent coding standards it's 2019 who the fuck still uses soft tabs?

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,10 +8,10 @@ window._ = require('lodash');
  */
 
 try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
+	window.Popper = require('popper.js').default;
+	window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap');
+	require('bootstrap');
 } catch (e) {}
 
 /**
@@ -33,9 +33,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+	window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+	console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
 /**
