c0ccf5d07116ff377af9acf5963923364fc36e17
Felipe Rodrigues <felipe@felipevr.com>
2019-03-21 11:16:40 -0300

    Add basic tests

--- tests/Unit/RequisitionTest.php
+++ tests/Unit/RequisitionTest.php
@@ -2,18 +2,20 @@
 
 namespace Tests\Unit;
 
+use \App\Requisition;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class RequisitionTest extends TestCase
 {
 	/**
 	 * A basic test example.
 	 *
 	 * @return void
 	 */
-	public function testBasicTest()
+	public function testCanInstantiateARequisition()
 	{
-		$this->assertTrue(true);
+		$req = new Requisition();
+		$this->assertNotNull($req);
 	}
 }
