17eb7a107a266b83eee9e3009a746ae2de9054e9
David Fugimoto <dsfugimoto@gmail.com>
2019-04-05 00:38:05 -0300

    Feat: adding a common layout structure to be extended by views

--- resources/views/requisition-form.blade.php
+++ resources/views/requisition-form.blade.php
@@ -1,78 +1,35 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="css/app.css">
+@extends('layouts.master')
 
-		<title>Formulário de Atendimento ao usuário - Nova Solicitação</title>
-	</head>
-	<body>
-		<header>
-			<div class="banner">
-				<div class="logo-dac">
-					<img src="images/logo_dac.svg" alt="" />
-				</div>
-				<div class="banner__title">
-					<h2>Sistemas Acadêmicos - Formulário de Atendimento ao usuário</h2>
-				</div>
-				<div class="logo-unicamp">
-					<img src="images/logo_unicamp.svg" alt="" height="50" />
-				</div>
-			</div>
-			<div class="navigation">
-				<div class="container">
-					<ul class="menu">
-						<li>
-							<a href="/list">
-								Minhas Solicitações
-							</a>
-						</li>
-						<li>
-							<a class="active" href="">
-								Nova Solicitação
-							</a>
-						</li>
-						<!-- <li>
-							<a href="#">
-								Sair
-							</a>
-						</li> -->
-					</ul>
-				</div>
-			</div>
-		</header>
-		<main>
-			<div class="container">
-				<h1>Nova Solicitação</h1>
-				<h6>(*) Campos Obrigatórios</h6>
-				<br>
-				<form action="/" method="POST">
-					@csrf
+@section('title', 'Nova Solicitação')
 
-					<div class="form-group">
-						<label for="topic">Assunto *</label>
-						<input class="form-control" type="text" name="topic" required>
-					</div>
-					<div class="form-group">
-						<label for="category">Categoria *</label>
-						<select class="form-control" name="category" required>
-							{{-- Placholder --}}
-							<option value="" selected disabled hidden>-- Selecione a Categoria --</option>
+@section('content')
+	<h1>Nova Solicitação</h1>
+	<h6>(*) Campos Obrigatórios</h6>
+	<br>
+	<form action="/" method="POST">
+		@csrf
 
-							@foreach($categories as $cat)
-								<option value="{{ $cat->id }}">{{ $cat->name }}</option>
-							@endforeach
-						</select>
-					</div>
-					<div class="form-group">
-						<label for="description">Descrição *</label>
-						<textarea class="form-control" name="description" rows="3" required></textarea>
-					</div>
+		<div class="form-group">
+			<label for="topic">Assunto *</label>
+			<input class="form-control" type="text" name="topic" required>
+		</div>
+		<div class="form-group">
+			<label for="category">Categoria *</label>
+			<select class="form-control" name="category" required>
+				{{-- Placholder --}}
+				<option value="" selected disabled hidden>-- Selecione a Categoria --</option>
 
-					<button class="btn btn-dac" type="submit">Enviar</button>
-				</form>
-			</div>
-		</main>
-	</body>
-</html>
+				@foreach($categories as $cat)
+					<option value="{{ $cat->id }}">{{ $cat->name }}</option>
+				@endforeach
+			</select>
+		</div>
+		<div class="form-group">
+			<label for="description">Descrição *</label>
+			<textarea class="form-control" name="description" rows="3" required></textarea>
+		</div>
+
+		<button class="btn btn-dac" type="submit">Enviar</button>
+	</form>
+</div>
+@stop
--- resources/views/requisition-list.blade.php
+++ resources/views/requisition-list.blade.php
@@ -1,72 +1,30 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="css/app.css">
+@extends('layouts.master')
 
-		<title>Formulário de Atendimento ao usuário - Minhas Solicitações</title>
-	</head>
-	<body>
-		<header>
-			<div class="banner">
-				<div class="logo-dac">
-					<img src="images/logo_dac.svg" alt="" />
-				</div>
-				<div class="banner__title">
-					<h2>Sistemas Acadêmicos - Formulário de Atendimento ao usuário</h2>
-				</div>
-				<div class="logo-unicamp">
-					<img src="images/logo_unicamp.svg" alt="" height="50" />
-				</div>
-			</div>
-			<div class="navigation">
-				<div class="container">
-					<ul class="menu">
-						<li>
-							<a class="active" href="">
-								Minhas Solicitações
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								Nova Solicitação
-							</a>
-						</li>
-						<!-- <li>
-							<a href="#">
-								Sair
-							</a>
-						</li> -->
-					</ul>
-				</div>
-			</div>
-		</header>
-		<main>
-			<div class="container">
-				<h1>Minhas Solicitações</h1>
-				<br>
-				<table class="table table-bordered">
-					<thead class="thead-light">
-						<tr>
-							<th>Número</th>
-							<th>Assunto</th>
-							<th>Categoria</th>
-							<th>Descrição</th>
-						<tr>
-					</thead>
-					<tbody>
-						@foreach ($requisitions as $req)
-							<tr>
-								<td>{{ $req->id }}</td>
-								<td>{{ $req->topic }}</td>
-								<td>{{ $req->category->name }}</td>
-								<td>{{ str_limit($req->description, $limit = 40, $end = '...') }}</td>
-							</tr>
-						@endforeach
-					</tbody>
-				</table>
-			</div>
-		</main>
-	</body>
-</html>
+@section('title', 'Minhas Solicitações')
+
+@section('content')
+	<div class="container">
+		<h1>Minhas Solicitações</h1>
+		<br>
+		<table class="table table-bordered">
+			<thead class="thead-light">
+				<tr>
+					<th>Número</th>
+					<th>Assunto</th>
+					<th>Categoria</th>
+					<th>Descrição</th>
+				<tr>
+			</thead>
+			<tbody>
+				@foreach ($requisitions as $req)
+					<tr>
+						<td>{{ $req->id }}</td>
+						<td>{{ $req->topic }}</td>
+						<td>{{ $req->category->name }}</td>
+						<td>{{ str_limit($req->description, $limit = 40, $end = '...') }}</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
+@stop
