cd75384036f889d75ce6457b7533fff3509360a4
Raphael Mobis Tacla <r.mobis@gmail.com>
2019-06-28 16:46:40 -0300

    make requisitions soft deletable

--- app/Requisition.php
+++ app/Requisition.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Requisition extends Model {
+	use SoftDeletes;
+
 	public function category() {
 		return $this->belongsTo(Category::class);
 	}
